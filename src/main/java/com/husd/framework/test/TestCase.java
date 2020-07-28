1dc10cf4c64ed969b0ae90d8e3eb27d1888c0d19
hushengdong <shengdonghu@126.com>
2020-07-28 15:21:56 +0800

    增加了1个简单题的解法

---
+++ src/main/java/com/husd/framework/test/TestCase.java
@@ -0,0 +1,13 @@
+package com.husd.framework.test;
+
+/**
+ * 这里是这个类的功能描述
+ *
+ * @author hushengdong
+ */
+public abstract class TestCase {
+
+    public abstract String valid();
+
+    public abstract String invalid();
+}
---
+++ src/main/java/com/husd/framework/test/money/MoneyTest.java
@@ -0,0 +1,21 @@
+package com.husd.framework.test.money;
+
+import com.husd.framework.test.TestCase;
+
+/**
+ * 这里是这个类的功能描述
+ *
+ * @author hushengdong
+ */
+public class MoneyTest extends TestCase {
+
+    @Override
+    public String valid() {
+        return null;
+    }
+
+    @Override
+    public String invalid() {
+        return null;
+    }
+}
