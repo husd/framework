87899d1407b7a66e60bd5fdc4d9e69b8bf9643b2
hushengdong <shengdonghu@126.com>
2020-12-21 23:14:51 +0800

    增加了1个杂技

---
+++ src/test/java/com/husd/framework/base/ArrayTest.java
@@ -0,0 +1,18 @@
+package com.husd.framework.base;
+
+import org.junit.Test;
+
+/**
+ * 这里是这个类的功能描述
+ *
+ * @author hushengdong
+ */
+public class ArrayTest {
+
+    @Test
+    public void test() {
+
+        int[] arr = {0, 1, 2, 3, 4, 5};
+        System.out.println(arr[-1]);
+    }
+}
